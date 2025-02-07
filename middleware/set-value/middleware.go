8b36b23c73be41509211dba2388827ca72c35fd6
Robert Hurst <robertwhurst@gmail.com>
2025-02-06 19:40:26 -0800

    fix double pointer on setvalue middleware

--- middleware/set-value/middleware.go
+++ middleware/set-value/middleware.go
@@ -2,7 +2,7 @@ package setvalue
 
 import "github.com/RobertWHurst/navaros"
 
-func Middleware[V *any](key string, value V) func(ctx *navaros.Context) {
+func Middleware[V any](key string, value *V) func(ctx *navaros.Context) {
 	return func(ctx *navaros.Context) {
 		ctx.Set(key, *value)
 		ctx.Next()
