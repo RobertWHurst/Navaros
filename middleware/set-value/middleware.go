e1804d50eb2ab8d2a24c55306b074ce6202c1d60
Robert Hurst <robertwhurst@gmail.com>
2025-02-06 17:27:44 -0800

    add set value middleware

---
+++ middleware/set-value/middleware.go
@@ -0,0 +1,10 @@
+package setfn
+
+import "github.com/RobertWHurst/navaros"
+
+func Middleware[V *any](key string, value V) func(ctx *navaros.Context) {
+	return func(ctx *navaros.Context) {
+		ctx.Set(key, *value)
+		ctx.Next()
+	}
+}
